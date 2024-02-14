221de9ac779fecff46744349ff183c98aa84f59b
Romain Dénarié <romain.denarie@exoplatform.com>
2024-02-14 16:30:59 +0100

    Add BasicAuthRestLoginModule

---
+++ services/src/main/java/org/exoplatform/addons/apidirectaccess/BasicAuthRestLoginModule.java
@@ -0,0 +1,69 @@
+package org.exoplatform.addons.apidirectaccess;
+
+import javax.servlet.http.HttpServletRequest;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.security.Authenticator;
+import org.exoplatform.services.security.Credential;
+import org.exoplatform.services.security.PasswordCredential;
+import org.exoplatform.services.security.UsernameCredential;
+import org.exoplatform.services.security.j2ee.TomcatLoginModule;
+import org.gatein.sso.agent.tomcat.ServletAccess;
+
+import javax.security.auth.login.LoginException;
+import java.util.Base64;
+
+public class BasicAuthRestLoginModule extends TomcatLoginModule {
+  private static final Log LOG = ExoLogger.getLogger(BasicAuthRestLoginModule.class);
+
+  @Override
+  public boolean login() throws LoginException {
+    try {
+      //todo :
+      // - add a property to explicitly activate BasicAuth
+      // - Change to a more robust solution (API token in settings for example)
+
+      HttpServletRequest servletRequest = ServletAccess.getRequest();
+      if (servletRequest == null) {
+        LOG.warn("HttpServletRequest is null. BasicAuthLoginModule will be ignored.");
+        return false;
+      }
+      String authorization = servletRequest.getHeader("Authorization");
+      if (servletRequest.getRemoteUser() == null &&
+          (servletRequest.getRequestURI().startsWith("/portal/rest/")
+          || servletRequest.getRequestURI().startsWith("/rest/"))
+          && authorization != null
+          && authorization.toLowerCase().startsWith("basic")) {
+
+        // Authorization: Basic base64credentials
+        String base64Credentials = authorization.substring("Basic".length()).trim();
+        byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+        String credentialsString = new String(credDecoded);
+        // credentials = username:password
+        final String[] values = credentialsString.split(":", 2);
+        String username = values[0];
+        String password = values[1];
+
+        if (username != null) {
+          Authenticator authenticator = getContainer().getComponentInstanceOfType(Authenticator.class);
+          if (authenticator == null) {
+            throw new LoginException("No Authenticator component found, check your configuration");
+          }
+          Credential[] credentials =
+              new Credential[] { new UsernameCredential(username), new PasswordCredential(password) };
+          if (authenticator.validateUser(credentials) != null) {
+            LOG.info("API Basic Auth successful");
+            identity = authenticator.createIdentity(username);
+            sharedState.put("javax.security.auth.login.name", username);
+            sharedState.put("exo.security.identity", identity);
+            subject.getPublicCredentials().add(new UsernameCredential(username));
+            return true;
+          }
+        }
+      }
+    } catch(Exception e){
+      throw new LoginException(e.getMessage());
+    }
+    return false;
+  }
+}
